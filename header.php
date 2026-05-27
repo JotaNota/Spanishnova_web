68bd49eb3600b7b90c3546ff9f8f7b30b5e0e6d8
dzespina <dzespina@gmail.com>
2026-05-27 14:58:30 -0300

    header_dropdown

--- header.php
+++ header.php
@@ -14,14 +14,71 @@
       <span><?php bloginfo('name'); ?></span>
     </a>
 
-    <nav class="center-nav" aria-label="Primary navigation">
+   <nav class="center-nav" aria-label="Primary navigation">
+  <ul class="sn-main-menu">
+
+    <li class="menu-item has-submenu">
       <a href="<?php echo esc_url(home_url('/grammar/')); ?>">Grammar</a>
+      <ul class="submenu">
+        <li><a href="<?php echo esc_url(home_url('/grammar/')); ?>">All grammar</a></li>
+        <li><a href="<?php echo esc_url(home_url('/grammar/verbs/')); ?>">Verbs</a></li>
+        <li><a href="<?php echo esc_url(home_url('/grammar/questions/')); ?>">Questions</a></li>
+        <li><a href="<?php echo esc_url(home_url('/grammar/negatives/')); ?>">Negatives</a></li>
+      </ul>
+    </li>
+
+    <li class="menu-item has-submenu">
       <a href="<?php echo esc_url(home_url('/vocabulary/')); ?>">Vocabulary</a>
+      <ul class="submenu">
+        <li><a href="<?php echo esc_url(home_url('/vocabulary/')); ?>">All vocabulary</a></li>
+        <li><a href="<?php echo esc_url(home_url('/vocabulary/food/')); ?>">Food</a></li>
+        <li><a href="<?php echo esc_url(home_url('/vocabulary/travel/')); ?>">Travel</a></li>
+        <li><a href="<?php echo esc_url(home_url('/vocabulary/daily-life/')); ?>">Daily life</a></li>
+      </ul>
+    </li>
+
+    <li class="menu-item has-submenu">
       <a href="<?php echo esc_url(home_url('/readings/')); ?>">Readings</a>
+      <ul class="submenu">
+        <li><a href="<?php echo esc_url(home_url('/readings/')); ?>">All readings</a></li>
+        <li><a href="<?php echo esc_url(home_url('/readings/beginner/')); ?>">Beginner</a></li>
+        <li><a href="<?php echo esc_url(home_url('/readings/intermediate/')); ?>">Intermediate</a></li>
+        <li><a href="<?php echo esc_url(home_url('/readings/biographies/')); ?>">Biographies</a></li>
+      </ul>
+    </li>
+
+    <li class="menu-item has-submenu">
       <a href="<?php echo esc_url(home_url('/conversations/')); ?>">Conversations</a>
+      <ul class="submenu">
+        <li><a href="<?php echo esc_url(home_url('/conversations/')); ?>">All conversations</a></li>
+        <li><a href="<?php echo esc_url(home_url('/conversations/everyday/')); ?>">Everyday</a></li>
+        <li><a href="<?php echo esc_url(home_url('/conversations/travel/')); ?>">Travel</a></li>
+        <li><a href="<?php echo esc_url(home_url('/conversations/restaurants/')); ?>">Restaurants</a></li>
+      </ul>
+    </li>
+
+    <li class="menu-item has-submenu">
       <a href="<?php echo esc_url(home_url('/practice/')); ?>">Practice</a>
+      <ul class="submenu">
+        <li><a href="<?php echo esc_url(home_url('/practice/')); ?>">All practice</a></li>
+        <li><a href="<?php echo esc_url(home_url('/practice/exercises/')); ?>">Exercises</a></li>
+        <li><a href="<?php echo esc_url(home_url('/practice/translation/')); ?>">Translation</a></li>
+        <li><a href="<?php echo esc_url(home_url('/practice/quizzes/')); ?>">Quizzes</a></li>
+      </ul>
+    </li>
+
+    <li class="menu-item has-submenu">
       <a href="<?php echo esc_url(home_url('/resources/')); ?>">Resources</a>
-    </nav>
+      <ul class="submenu">
+        <li><a href="<?php echo esc_url(home_url('/resources/')); ?>">All resources</a></li>
+        <li><a href="<?php echo esc_url(home_url('/resources/pdfs/')); ?>">PDFs</a></li>
+        <li><a href="<?php echo esc_url(home_url('/resources/worksheets/')); ?>">Worksheets</a></li>
+        <li><a href="<?php echo esc_url(home_url('/resources/checklists/')); ?>">Checklists</a></li>
+      </ul>
+    </li>
+
+  </ul>
+</nav>
 
     <div class="top-actions">
       <a href="<?php echo esc_url(home_url('/classes/')); ?>">Classes</a>
