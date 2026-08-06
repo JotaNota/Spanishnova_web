93b809b870080fd92d6b6374073a4898bd850bd4
JotaNota <dzespina@gmail.com>
2026-08-06 14:24:43 -0300

    Load default template for new grammar posts

--- functions.php
+++ functions.php
@@ -7,6 +7,7 @@ require_once get_template_directory() . '/inc/cpt.php';
 require_once get_template_directory() . '/inc/taxonomies.php';
 require_once get_template_directory() . '/inc/taxonomy-seeds.php';
 require_once get_template_directory() . '/inc/grammar-meta.php';
+require_once get_template_directory() . '/inc/grammar-editor-template.php';
 
 if (!function_exists('spanishnova_get_card_excerpt')) {
     function spanishnova_get_card_excerpt($post_id = null) {
